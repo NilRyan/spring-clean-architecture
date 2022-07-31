c77b046586027a4fe3f97a238cc9044c959f12e1
Neil Ryan L <neilryanlipaod@gmail.com>
2022-07-31 16:15:06 +0800

    initial commit

---
+++ src/main/java/dev/indifferentcat/clean_arch/CleanArchitectureImplementationSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.indifferentcat.clean_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureImplementationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureImplementationSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/indifferentcat/clean_arch/CleanArchitectureImplementationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.indifferentcat.clean_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureImplementationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
